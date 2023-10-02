37ff1afbee4562f6f38c03b40d64499e7ed3eb43
Daniel La Rocque <dlarocque.contact@gmail.com>
2023-10-02 10:29:31 -0500

    Frontend, backend, db, Docker setup (#44)
    Initial set up for the React frontend, Express backend, PostgreSQL database + Prisma, Docker, and VSCode development environment (ESLint + Prettier).

---
+++ server/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'eslint-config-prettier',
+    'plugin:prettier/recommended',
+  ],
+};
