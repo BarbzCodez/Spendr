c5c65e6378ecae575450bcbe4a9afea52edcfa2f
Daniel La Rocque <larocq17@myumanitoba.ca>
2023-10-05 18:51:39 -0500

    Initial authentication API, Docker broken

---
+++ server/src/middleware/authenticate.ts
@@ -0,0 +1,41 @@
+// src/middleware/authenticate.ts
+import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+const JWT_SECRET = process.env.JWT_SECRET!; // Store this securely, maybe in environment variables
+
+/**
+ * Middleware to authenticate a user
+ *
+ * This middleware will check for a token in the Authorization header.
+ * If the token is valid, it will add the user's id to the request object.
+ *
+ * @param req Request object
+ * @param res Response object
+ * @param next Next function
+ * @returns void
+ */
+export const authenticate = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+
+  if (!token) {
+    return res.status(401).send({ error: 'Token not provided' });
+  }
+
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: 'Token invalid' });
+    }
+
+    if (!decoded || typeof decoded === 'string') {
+      return res.status(401).send({ error: 'Token invalid' });
+    }
+
+    req.userId = decoded.id;
+    next();
+  });
+};
---
+++ server/src/test/unit/middleware/authenticate.test.ts
@@ -0,0 +1,80 @@
+import jwt from 'jsonwebtoken';
+import { authenticate } from '../../../middleware/authenticate'; // path to your authenticate middleware
+import { NextFunction, Request, Response } from 'express';
+
+// Mocking jwt.verify method
+jest.mock('jsonwebtoken');
+
+describe('authenticate middleware', () => {
+  let req: Partial<Request>;
+  let res: Partial<Response>;
+  let next: NextFunction;
+
+  beforeEach(() => {
+    // Setting up mock request, response, and next objects
+    req = {};
+    res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    };
+    next = jest.fn();
+  });
+
+  it('responds with 401 if no token is provided', () => {
+    req.header = jest.fn().mockReturnValue(null);
+    authenticate(req as Request, res as Response, next);
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.send).toHaveBeenCalledWith({ error: 'Token not provided' });
+  });
+
+  it('responds with 401 if an invalid token is provided', () => {
+    req.header = jest.fn().mockReturnValue('Bearer fake_token');
+    (jwt.verify as jest.Mock).mockImplementation((token, secret, cb) =>
+      cb(true, null),
+    ); // simulating an error
+
+    authenticate(req as Request, res as Response, next);
+
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.send).toHaveBeenCalledWith({ error: 'Token invalid' });
+  });
+
+  it('responds with 401 if the decoded token is a string', () => {
+    req.header = jest.fn().mockReturnValue('Bearer some_token');
+    (jwt.verify as jest.Mock).mockImplementation(
+      (token, secret, cb) => cb(null, 'string_payload'), // simulating a string decoded payload
+    );
+
+    authenticate(req as Request, res as Response, next);
+
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.send).toHaveBeenCalledWith({ error: 'Token invalid' });
+  });
+
+  it('calls next function if a valid token is provided', () => {
+    req.header = jest.fn().mockReturnValue('Bearer valid_token');
+    (jwt.verify as jest.Mock).mockImplementation((token, secret, cb) =>
+      cb(null, { id: '123' }),
+    ); // simulating a valid decode
+
+    authenticate(req as Request, res as Response, next);
+
+    expect(req.userId).toBe('123');
+    expect(next).toHaveBeenCalled();
+  });
+
+  it('decodes the userId from a valid JWT', () => {
+    const userId = '123';
+    const mockToken = jwt.sign({ id: userId }, process.env.JWT_SECRET!);
+    req.header = jest.fn().mockReturnValue(`Bearer ${mockToken}`);
+
+    authenticate(req as Request, res as Response, next);
+
+    expect(req.userId).toBe(userId);
+    expect(next).toHaveBeenCalled();
+  });
+});
+
+afterAll(() => {
+  jest.restoreAllMocks();
+});
