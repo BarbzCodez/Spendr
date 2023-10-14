cd9a185c6b8a1c695b89b963942348de235f21d9
VictoriaKGN <koganv@myumanitoba.ca>
2023-10-14 17:03:22 -0500

    signup refactor

--- client/src/interfaces/interfaces.ts
+++ client/src/interfaces/interfaces.ts
@@ -2,14 +2,6 @@ export interface LoggedInProps {
   isLoggedIn: boolean;
 }
 
-export interface SignupFields {
-  username: string;
-  password: string;
-  confirmPassword: string;
-  securityQuestion: string;
-  securityAnswer: string;
-}
-
 export interface SignupVals {
   username: string;
   password: string;
