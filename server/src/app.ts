8f8d0c890ce85b72311ad631b735cd3b82f4de5f
Ethan Ducharme <ethanducharm@gmail.com>
2023-10-16 00:36:20 -0500

    Created error logging
    Errors are logged for user and system issues that occur within the server. The logs are found under server/logs

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import userRoutes from './routes/auth';
 import expenseRoutes from './routes/expenses';
+import { logError } from './middleware/errorHandling';
 
 // Initialize express
 const app = express();
@@ -9,6 +10,13 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.on('finish', () => {
+    logError(req, res, next);
+  });
+  next();
+});
 
 // Routes
 app.use('/users', userRoutes);
