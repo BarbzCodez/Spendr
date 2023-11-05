889cbef035c8d680789f0059ca9693d8ea165924
Daniel La Rocque <larocq17@myumanitoba.ca>
2023-11-04 22:01:29 -0500

    Return 400 on validation errors when updating expense

--- server/src/routes/expenses.ts
+++ server/src/routes/expenses.ts
@@ -120,6 +120,11 @@ router.put(
     .isIn(Object.values(ExpenseCategory))
     .withMessage('Category must be a valid category'),
   async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     const { expenseId } = req.params;
 
     try {
