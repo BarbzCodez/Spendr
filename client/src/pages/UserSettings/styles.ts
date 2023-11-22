4332836fd18cecde4bdd0ece070972e0d5373ded
VictoriaKGN <koganv@myumanitoba.ca>
2023-11-22 16:05:27 -0600

    user settings height fix

--- client/src/pages/UserSettings/styles.ts
+++ client/src/pages/UserSettings/styles.ts
@@ -2,8 +2,9 @@ import { styled, Stack } from '@mui/system';
 
 export const SettingsStack = styled(Stack)(({ theme }) => ({
   width: '50%',
-  height: '70vh',
+  height: '55vh',
   padding: '2rem',
   backgroundColor: `${theme.palette.secondary.main}80`,
   borderRadius: '16px',
+  marginBottom: '15vh',
 }));
